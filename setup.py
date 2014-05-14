96ce88752eee0de86076d8a13ba8f3b6b0631299
Park Hyunwoo <ez@smartstudy.co.kr>
2014-05-14 19:58:35 +0900

    Bump to 0.1.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ classifiers = [
 
 setup(
     name='hog',
-    version='0.0.1',
+    version='0.1.0',
     py_modules=['hog'],
     author='Park Hyunwoo',
     author_email='ez.amiryo' '@' 'gmail.com',
