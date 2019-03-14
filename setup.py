8172dbb10bc140653ece8f241be68653e799cc05
Park Hyunwoo <ez.amiryo@gmail.com>
2019-03-14 17:11:05 +0900

    Apply changed function name

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'hog = hog.hog:main',
+            'hog = hog.hog:hog',
         ],
     },
 )
